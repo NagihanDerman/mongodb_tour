76b0d0cb16953cda72c66f82d7e0e9d43862052a
NagihanDerman <dermannagihan@gmail.com>
2024-12-05 15:50:32 +0100

    authorization login signup hash ans salt

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,6 @@
 const { Schema, default: mongoose } = require("mongoose");
 const validator = require("validator");
-
+const bcrypt = require("bcrypt");
 
 
 // Kullanıcı Şeması
@@ -9,7 +9,7 @@ const userSchema = new Schema({
     type: String,
     required: [true, "Kullanıcı isim değerine sahip olmalıdır"],
     minLength: [3, "Kullanıcı ismi en az 3 karakter olmalı"],
-    maxLength: [30, "Kullanıcı ismi en fazla 30 karakter olabilir"],
+    maxLength: [30, "Kullanıci ismi en fazla 30 karakter olabilir"],
   },
 
   email: {
@@ -55,11 +55,33 @@ const userSchema = new Schema({
 
  
 });
+//? Veritbanına kullanıcıyı kaydetmeden önce:
+//* passwordConfirm alanını kaldır
+//* password alanını şifreleme algoritmaları ile şifrele
+userSchema.pre("save", async function (next) {
+  // save kullanıcı belgesi her güncellendiğinde çalışır ama parola değişmediyse aşağıdaki kodlar çalışmamalı
+  if (!this.isModified("password")) return next();
+
+  // şifreyi saltla ve hashle
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // onay şifresini kaldır
+  this.passwordConfirm = undefined;
+
+  next();
+});
+//? Sadece model üzerinden erişilebilen fonksiyon
+// normal şifre ile hesahlenmiş şifreyi karşılaştırsın
+userSchema.methods.correctPass = async function (pass, hashedPass) {
+  // pass > Denem@123
+  // hashedPass > $2b$12$N9BQoexMwICIptP7t5nMJOGqNGJB03qad38G19qe5tTZjOi0th.fi
+  return await bcrypt.compare(pass, hashedPass);
+};
+
+
 
 
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
-
-
 
